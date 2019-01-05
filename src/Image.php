8cb70b35a13b5219e28152f4298be50941b4ca37
Galifax <galifax94@gmail.com>
2019-01-05 14:46:31 +0200

    first commit

---
+++ src/Image.php
@@ -0,0 +1,66 @@
+<?php
+namespace Galifax;
+
+// Класс загрузки изображения
+class Image
+{
+    protected static $path; // Временное хранение изображения
+
+    protected static $name; // название изображения
+
+    // Метод загрузки фпйла в файловую систему
+    public static function download($url = '')
+    {
+        if ($url != '') {
+            $img_url = $url; // Cсылка на картинку
+            $Headers = @get_headers($img_url); // Волучаем заголовки из ответа сервера на HTTP-запрос
+
+            // Проверка на существование картинки
+            if (preg_match("|200|", $Headers[0])) {
+                // Проверка на существование картинки
+                $image = file_get_contents($img_url); // Получение информации об изображении
+                $ext = substr($url, -3); // Получение формата изображения
+
+                self::$name = uniqid() . '.' . $ext;
+                // Проверка доступного фррмата изображения
+                if ($ext == 'png' || $ext == 'jpg' || $ext == 'gif') {
+                    self::save($image, $ext); // Метод сохранения изображения
+
+                } else {
+                    throw new \Exception('Недоступный формат');
+                }
+            } else {
+                // Ошибка, если картинка не найдена
+                throw new \Exception('Изображение не найдено');
+            }
+        } else {
+            throw new \Exception('Cсылка не должна быть пустой');
+        }
+    }
+
+    // Метод сохранения изображения
+    public function save($image, $ext)
+    {
+        // Путь для временного хранения изображения
+        $path = __DIR__ . '/../static/' . self::$name;
+        // Сохранение картинки на диск
+        file_put_contents($path, $image);
+        // Заставляем браузерпоказать окно сохранения файла
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename=' . basename(self::$name));
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($path));
+        // Чтение изображения
+        readfile($path);
+
+        // Удаление мзображение с диска
+        unlink($path);
+        // Прекращение выполнение скрипта
+        return true;
+        exit;
+    }
+}
---
+++ tests/unit/ImageTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use \PHPUnit\Framework\TestCase;
+use Galifax\Image;
+
+class ImageTest extends TestCase 
+{
+    protected $backupGlobalsBlacklist = ['globalVariable'];
+
+    /**
+     * @runInSeparateProcess
+     */
+    // Проверка на скачивание изображения с рабочей ссылкой
+    public function testDownload()
+    {
+        $this->assertNull(Image::download('https://st2.depositphotos.com/2001755/5408/i/450/depositphotos_54081723-stock-photo-beautiful-nature-landscape.jpg'));
+    }
+
+    // Тест исключения на пустую ссылку
+    public function testExceptionEmptyUrl()
+    {
+        $this->expectException(Exception::class);
+        // Пустая ссылка
+        Image::download();
+    }
+
+    // Тест исключения на страницу 404
+    public function testExceptionNotFoundUrl()
+    {
+        $this->expectException(Exception::class);
+        // // Неверная ссылка
+        Image::download('https://st2.depositphotos.com/full-nature-landscape.jpg');
+    }
+
+    // Тест исключения неверный формат изобоажения
+    public function testExceptionInvalidFormat()
+    {
+        $this->expectException(Exception::class);
+        // // Неверный формат(svg)
+        image::download('https://s.cdpn.io/3/kiwi.svg');
+    }
+    
+}
